403b1d6219c5138b7911ab31b8d32f3b06deaf09
httpssimonw <kimasemka0@gmail.com>
2026-09-22 01:19:06 +0900

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,31 @@
+// #include <stdio.h>
+
+
+// int main()
+// {  
+//     int score;
+//     char grades;
+
+//     scanf("%d", &score);
+
+//     switch   (score/10){
+//      case 10: 
+//      case 9:
+//         grades = 'A';
+//         break;
+//      case 8:
+//         grades = 'B';
+//         break;
+//      case 7:
+//         grades = 'C';
+//         break;
+//      case 6:
+//         grades = 'D';
+//         break;
+//      default:
+//         grades = 'F';
+//     }
+
+//     printf("%c\n", grades);
+//     return 0;
+// }
